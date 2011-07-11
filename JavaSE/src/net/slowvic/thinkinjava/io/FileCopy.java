c53ff25b88d110c30d8e79dba3940f132a475ff6
steinyxu <steinyxu@gmail.com>
2011-07-11 06:17:52 +0000

    使用nio拷贝文件，注解基本用法

---
+++ JavaSE/src/net/slowvic/thinkinjava/io/FileCopy.java
@@ -0,0 +1,40 @@
+package net.slowvic.thinkinjava.io;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * 本例使用nio中的FileChannel类来实现一个简单的文件拷贝工作
+ * 注意ByteBuffer的使用
+ */
+public class FileCopy {
+	public static void copyFile(String from,String to) throws IOException
+	{
+		FileChannel fic = new FileInputStream(from).getChannel();
+		FileChannel foc = new FileOutputStream(to).getChannel();
+		
+		ByteBuffer buffer = ByteBuffer.allocate(1024);
+		
+		while(true)
+		{
+			buffer.clear();
+			int r = fic.read(buffer);
+			if(r==-1)
+			{
+				break;
+			}
+			buffer.flip();
+			foc.write(buffer);
+		}
+		fic.close();
+		foc.close();
+	}
+	
+	public static void main(String[] args) throws IOException
+	{
+		copyFile("E:/个人使用/摘录.txt","E:/个人使用/摘录.sql");
+	}
+}
