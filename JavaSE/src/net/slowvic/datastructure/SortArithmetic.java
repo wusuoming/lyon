ef785ee8362e2e90459d76555d712c91ca348dfc
steinyxu <steinyxu@gmail.com>
2011-09-05 09:31:20 +0000

    快排算法

--- JavaSE/src/net/slowvic/datastructure/SortArithmetic.java
+++ JavaSE/src/net/slowvic/datastructure/SortArithmetic.java
@@ -1,5 +1,7 @@
 package net.slowvic.datastructure;
 
+import java.util.Arrays;
+
 public class SortArithmetic {
 
 	public static <E> void printArr(E[] arr) {
@@ -23,7 +25,25 @@ public class SortArithmetic {
 		return arr;
 	}
 
+	// 希尔排序
+	public static <E extends Comparable<? super E>> E[] shellSort(E[] arr) {
+		int j;
+		for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+			for (int i = gap; i < arr.length; i++) {
+				E temp = arr[i];
+				for (j = i; j >= gap && temp.compareTo(arr[j - gap]) < 0; j -= gap) {
+					arr[j] = arr[j - gap];
+				}
+			}
+		}
+		return arr;
+	}
+
 	public static void main(String[] args) {
-		printArr(insertionSort(new Integer[] { 6, 3, 49, 16, 68, 27 }));
+		//printArr(insertionSort(new Integer[] { 6, 3, 49, 16, 68, 27 }));
+		//printArr(shellSort(new Integer[] { 6, 3, 49, 16, 68, 27 }));
+		Integer[] arr = new Integer[] { 6, 3, 49, 16, 68, 27 };
+		Arrays.sort(arr);
+		printArr(arr);
 	}
 }
