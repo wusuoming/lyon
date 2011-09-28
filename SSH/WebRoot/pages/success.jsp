481e2cd462ed8135fe88a574cb6da4dcf556c6ad
steinyxu <steinyxu@gmail.com>
2011-09-28 15:49:08 +0000

    去除ActionForm，直接Request-->Modal

--- SSH/WebRoot/pages/success.jsp
+++ SSH/WebRoot/pages/success.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean"%>
+<%@ taglib uri="http://struts.apache.org/tags-html" prefix="html"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
@@ -12,6 +13,14 @@
 	<h2 align="center">
 		<bean:message key="reg.success"/>
 	</h2>
-	<c:out value="${msg}" />
+	您的注册信息如下
+	<hr>
+	姓名：<c:out value="${user.userName}" /><br>
+	出生年月：<c:out value="${user.bornYear}" /><br>
+	性别：<input type="radio" name="gender" value="male" checked="<c:out value='${user.gender=="male"}'/>"/>男
+		  <input type="radio" name="gender" value="female" checked=/>女<br>
+	密码：<c:out value="${user.password}" /><br>
+	邮箱：<c:out value="${user.email}" /><br>
+	爱好：<c:out value="${user.favorites}" /><br>
 </body>
 </html>
--- SSH/src/net/slowvic/web/action/RegAction.java
+++ SSH/src/net/slowvic/web/action/RegAction.java
@@ -3,30 +3,38 @@ package net.slowvic.web.action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import net.slowvic.web.form.UserForm;
+import net.slowvic.web.domainmodal.User;
 
+import org.apache.commons.beanutils.BeanUtils;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.actions.DispatchAction;
 
+/**
+ * 继承DispatchAction。注意不要Override掉execute方法。
+ * 
+ * @author xus
+ * <p>
+ */
 public class RegAction extends DispatchAction {
 
-	@Override
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		// BookDao bd = new BookDao();
-		// bd.crud();
-		request.setAttribute("msg", "欢迎登录");
-		return mapping.findForward("success");
-	}
+    public ActionForward success(ActionMapping mapping, ActionForm form,
+        HttpServletRequest request, HttpServletResponse response)
+        throws Exception {
+        // BookDao bd = new BookDao();
+        // bd.crud();
+        request.setAttribute("msg", "欢迎登录");
+        return mapping.findForward("success");
+    }
 
-	public ActionForward reg(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
-		UserForm userForm = (UserForm) form;
-		userForm.getPassword();
-		return mapping.findForward("success");
-	}
+    public ActionForward reg(ActionMapping mapping, ActionForm form,
+        HttpServletRequest request, HttpServletResponse response)
+        throws Exception {
+        User user = new User();
+        //将表单数据copy到模型类User中
+        BeanUtils.copyProperties(user, request.getParameterMap());
+        request.setAttribute("user", user);
+        return mapping.findForward("success");
+    }
 }
