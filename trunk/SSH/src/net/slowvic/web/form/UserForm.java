178feeb91e84445e312c24a0a5d7bffef6b481ae
steinyxu@gmail.com <steinyxu@gmail.com@1fb80924-1145-0ead-2bdc-cfb93208ff90>
2011-09-28 09:26:59 +0000

    Form绑定Action

--- trunk/SSH/src/net/slowvic/web/form/UserForm.java
+++ trunk/SSH/src/net/slowvic/web/form/UserForm.java
@@ -7,8 +7,11 @@ public class UserForm extends ActionForm {
 	private static final long serialVersionUID = -9057424334553447659L;
 
 	private String userName;
+	private int bornYear;
+	private String gender;
 	private String password;
 	private String email;
+	private String[] favorites;
 
 	public String getUserName() {
 		return userName;
@@ -16,6 +19,22 @@ public class UserForm extends ActionForm {
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public int getBornYear() {
+		return bornYear;
+	}
+
+	public void setBornYear(int bornYear) {
+		this.bornYear = bornYear;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 
 	public String getPassword() {
@@ -33,4 +52,13 @@ public class UserForm extends ActionForm {
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public String[] getFavorites() {
+		return favorites;
+	}
+
+	public void setFavorites(String[] favorites) {
+		this.favorites = favorites;
+	}
+
 }
