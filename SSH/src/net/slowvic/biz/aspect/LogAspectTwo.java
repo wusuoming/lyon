de511d5cfce869ffebd5dcacdacaff6bbb70ec29
steinyxu <steinyxu@gmail.com>
2011-10-30 16:41:22 +0000

    去掉ant构件文件，修改aspect

--- SSH/src/net/slowvic/biz/aspect/LogAspectTwo.java
+++ SSH/src/net/slowvic/biz/aspect/LogAspectTwo.java
@@ -1,19 +1,22 @@
 package net.slowvic.biz.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 @Aspect
 public class LogAspectTwo {
-	
-	@Pointcut("execution(* show*(..))")
-	public void logPointcut(){
-		
-	}
-	
-	@Before(value = "logPointcut()")
-	public void doBeforeOne(){
-		System.out.println("do before one");
-	}
+
+    @Pointcut("execution(* show*(..))")
+    public void logPointcut() {
+
+    }
+
+    @Around(value = "logPointcut()")
+    public void doBeforeOne(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("开始执行");
+        joinPoint.proceed();
+        System.out.println("执行完成");
+    }
 }
--- SSH/src/net/slowvic/util/SpringBeanUtil.java
+++ SSH/src/net/slowvic/util/SpringBeanUtil.java
@@ -4,8 +4,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class SpringBeanUtil {
+    /**
 	private static ApplicationContext applicationContext = new FileSystemXmlApplicationContext(
 			"WebRoot/WEB-INF/spring-conf.xml");
+	**/
+	
+	private static ApplicationContext applicationContext = new FileSystemXmlApplicationContext(
+	    "WebRoot/WEB-INF/springConfWithNoWeb.xml");
 
 	public static <T> T getBean(Class<T> clz) {
 		return applicationContext.getBean(clz);
