8bfd35e622f6855fa27faa21c5ff261b0f964e7b
steinyxu <steinyxu@gmail.com>
2011-12-20 00:47:56 +0000

    单链表

---
+++ datastructer/src/net/slowvic/datastructer/linklist/Node.java
@@ -0,0 +1,31 @@
+package net.slowvic.datastructer.linklist;
+
+public class Node {
+	private int data;
+	private Node next;
+
+	public Node(int data) {
+		this.data = data;
+	}
+
+	public Node(int data, Node next) {
+		this.data = data;
+		this.next = next;
+	}
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+}
