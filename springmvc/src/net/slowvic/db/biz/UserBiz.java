9be7315ab892149d99101be9e300d48cb92688e9
steinyxu <steinyxu@gmail.com>
2013-01-29 09:29:36 +0000

    springmvc：增加myBatis分页功能

--- springmvc/src/net/slowvic/db/biz/UserBiz.java
+++ springmvc/src/net/slowvic/db/biz/UserBiz.java
@@ -1,8 +1,13 @@
 package net.slowvic.db.biz;
 
+import java.util.List;
+
 import net.slowvic.db.dao.UserDao;
+import net.slowvic.db.page.PageInfo;
 import net.slowvic.domain.User;
 
+import org.noo.pagination.page.Page;
+import org.noo.pagination.page.Pagination;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,5 +30,23 @@ public class UserBiz {
 
     public User getUser(String userName, User user) {
         return dao.getUser(userName, user);
+    }
+
+    public List<User> getAllUsers() {
+        Page page = new Pagination();
+        page.setPageSize(1);
+        List<User> users = dao.findAllUsers(page);
+        return users;
+    }
+
+    public List<User> getAllUsers(PageInfo<User> pageInfo) {
+        List<User> users = dao.findAllUsersByAnnotation(pageInfo);
+        return users;
+    }
+
+    public List<User> getAllUsers(PageInfo<User> pageInfo, String userName) {
+        List<User> users = dao.findAllUsersByAnnotationAndParam(pageInfo,
+            userName);
+        return users;
     }
 }
--- springmvc/src/net/slowvic/db/dao/UserDao.java
+++ springmvc/src/net/slowvic/db/dao/UserDao.java
@@ -1,8 +1,12 @@
 package net.slowvic.db.dao;
 
+import java.util.List;
+
+import net.slowvic.db.page.PageInfo;
 import net.slowvic.domain.User;
 
 import org.apache.ibatis.annotations.Param;
+import org.noo.pagination.page.Page;
 
 public interface UserDao {
     User getUserById(int id);
@@ -10,4 +14,11 @@ public interface UserDao {
     User getUser(@Param("userName") String userName, @Param("user") User user);
 
     void addUser(User user);
+
+    List<User> findAllUsers(Page page);
+
+    List<User> findAllUsersByAnnotation(PageInfo<User> pageInfo);
+
+    List<User> findAllUsersByAnnotationAndParam(PageInfo<User> pageInfo,
+        @Param("userName") String userName);
 }
