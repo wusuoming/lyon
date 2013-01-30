e090493413d686ba4f1cd563dbc989b41c65c0ca
steinyxu@gmail.com <steinyxu@gmail.com@1fb80924-1145-0ead-2bdc-cfb93208ff90>
2013-01-30 15:46:36 +0000

    springmvc：增加junit测试

---
+++ trunk/springmvc/src/net/slowvic/test/junit/TestSuite.java
@@ -0,0 +1,16 @@
+package net.slowvic.test.junit;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ UserDbTest.class })
+/**
+ * 测试套件
+ * @author xus
+ * <p>
+ */
+public class TestSuite {
+
+}
